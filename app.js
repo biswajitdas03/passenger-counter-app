7eec4d137afb23ed3b08d574f40f15a3ab96192c
biswajitdas03 <biswajitdas0916@gmail.com>
2024-10-15 03:21:15 +0530

    removed comments from js

--- app.js
+++ app.js
@@ -16,17 +16,3 @@ resetBtn.addEventListener('click', function() {
     countValue.textContent = count;
 });
 
-// let count = 0;
-// const countValue = document.getElementById('count-value');
-// const incrementBtn = document.getElementById('increment-btn');
-// const resetBtn = document.getElementById('reset-btn');
-
-// incrementBtn.addEventListener('click',function() {
-//     count++;
-//     countValue.textContent = count;
-// });
-// resetBtn.addEventListener('click', function() {
-//     count = 0;
-//     countValue.textContent = count;
-// });
-
